f66447cc56d4e938b24ddc5e3ad316b38c496dbe
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-06 15:59:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.innerHTML = "Member";
+    member.style.color = "white";
+    member.style.backgroundColor = "black";
+    member.style.borderRadius = "10px";
+    member.style.padding = "5px";
+    member.style.margin = "5px";
+    member.style.fontSize = "20px";
+    member.style.fontFamily = "Arial";
+    member.style.fontWeight = "bold";
+    member.style.textAlign = "center";
+    member.style.width = "100px";
+    member.style.height = "50px";
+    member.style.lineHeight = "50px";
+    member.style.boxShadow = "0px 0px 10px black";
+    member.style.transition = "all 1s";
+}
